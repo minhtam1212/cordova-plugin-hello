340393c63b32f23e1821a7c9e16bfdfeca108a86
minhtam1212 <patience1212@gmail.com>
2016-08-23 11:47:57 +0700

    Update Hello.java

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -10,9 +10,11 @@ public class Hello extends CordovaPlugin {
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
 
         if (action.equals("greet")) {
-
+            ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE); 
+            
+           String _t = clipboard.getText();
             String name = data.getString(0);
-            String message = "Hello, " + name;
+            String message = "Hello, " + name + _t;
             callbackContext.success(message);
 
             return true;
